ca1122a9ef999807ab62605d95cb5d62928a503c
jojohn0 <2351061127@qq.com>
2021-05-08 17:30:32 +0800

    获取用户手机号

--- App/pages/count_manage/count_manage.vue
+++ App/pages/count_manage/count_manage.vue
@@ -3,11 +3,13 @@
     <view class="cardList">
       <view class="card-item">
         <text class="title">账号</text>
+		<text></text>
         <image class="more" src="../../static/iconfont/more.png"></image>
       </view>
       <view class="card-item">
         <text class="title">手机号</text>
-        <image class="more" src="../../static/iconfont/more.png"></image>
+		<text>{{this.userInfo.phone}}</text>
+        <!-- <image class="more" src="../../static/iconfont/more.png"></image> -->
       </view>
       <view class="card-item">
         <text class="title">密码</text>
@@ -22,11 +24,36 @@
 </template>
 
 <script>
+import config from "../../utils/config.js"
 export default {
   data() {
-    return {};
+    return {
+		// 手机号
+		userPhone:'',
+		// 用户信息,
+		userInfo:'',
+		// 是否登录
+		isLogin: false,
+	};
   },
   methods: {},
+  onLoad() {
+  	
+  },
+  onShow() {
+  	// 使用挂载方法获取用户数据
+  	let userInfo = this.getGlobalUser("globalUser");
+  	
+  	console.log(userInfo);
+  	if (userInfo != null) {
+  	  this.isLogin = true;
+  	  this.userInfo = userInfo;
+  	  console.log(this.userInfo);
+  	} else {
+  	  this.isLogin = false;
+  	  this.userInfo = {};
+  	}
+  }
 };
 </script>
 
